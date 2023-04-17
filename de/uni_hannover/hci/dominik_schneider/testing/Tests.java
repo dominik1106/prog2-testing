8ab8a2efc973abd9bd2fabb13914b7a32ceaca86
Dominik Schneider <dominik.p.s@outlook.de>
2023-04-17 17:21:41 +0200

    added testWithinDeviation to test doubles

--- de/uni_hannover/hci/dominik_schneider/testing/Tests.java
+++ de/uni_hannover/hci/dominik_schneider/testing/Tests.java
@@ -28,6 +28,23 @@ public class Tests {
         return false;
     }
 
+    public boolean testWithinDeviation(double result, double expected, double epsilon) {
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[2];
+
+        System.out.print("In " + caller.getClassName() + "." + caller.getMethodName() + ", line " + caller.getLineNumber() + ": ");
+
+        double difference = Math.abs(result - expected);
+
+        runTests++;
+        if(difference <= epsilon) {
+            System.out.println("Test passed.");
+            successTests++;
+            return true;
+        }
+        System.out.println("Actual value " + result + " is not within " + epsilon + " of expected value " + expected);
+        return false;
+    }
+
     public Tests() {
         Runtime.getRuntime().addShutdownHook(exitThread);
     }
